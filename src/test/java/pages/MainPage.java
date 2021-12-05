6eb0d8fe8ef4cd714e890b085dbe9bc203676df2
Alina Kashnikava <alina_kashnikava@epam.com>
2021-12-05 20:06:59 +0300

    Initial commit

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.testng.Assert.assertEquals;
+
+public class MainPage extends BasePage {
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+
+    private By pasteField = By.id("postform-text");
+    private By pasteExpirationPushing = By.id("select2-postform-expiration-container");
+    private By pasteExpirationSelection = By.xpath("//li[text()='10 Minutes']");
+    private By pasteNameFieldPushing = By.id("postform-name");
+    private By newPasteButton = By.xpath("//button[@class='btn -big']");
+    public void openPage(){
+        driver.get("https://pastebin.com");
+    }
+
+    public void writeTextInToNewPasteField(String textForNewPaste){
+        driver.findElement(pasteField).sendKeys(textForNewPaste);
+    }
+
+    public void selectPasteExpiration(){
+        driver.findElement(pasteExpirationPushing).click();
+        driver.findElement(pasteExpirationSelection).click();
+    }
+
+    public void writeTextInToPasteNameField(String textForPasteName){
+        driver.findElement(pasteNameFieldPushing).sendKeys(textForPasteName);
+    }
+
+    public void pressCreateNewPasteButton(){
+        driver.findElement(newPasteButton).click();
+    }
+}
---
+++ src/test/java/tests/MainPageTests.java
@@ -0,0 +1,15 @@
+package tests;
+import org.testng.annotations.Test;
+
+public class MainPageTests extends BaseTest{
+
+    @Test
+    public void createNewPaste(){
+        mainPage.openPage();
+        mainPage.writeTextInToNewPasteField("Hello from WebDriver");
+        mainPage.selectPasteExpiration();
+        mainPage.writeTextInToPasteNameField("helloweb");
+        mainPage.pressCreateNewPasteButton();
+                   }
+}
+
